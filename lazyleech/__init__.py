01c91fb1a251ba5743402ccdf83fe3503f6b5358
Gautam Saini <73359023+Gautam1834@users.noreply.github.com>
2021-08-01 02:24:34 +0530

    Update __init__.py

--- lazyleech/__init__.py
+++ lazyleech/__init__.py
@@ -27,9 +27,9 @@ TESTMODE = os.environ.get('TESTMODE')
 TESTMODE = TESTMODE and TESTMODE != '0'
 
 EVERYONE_CHATS = os.environ.get('EVERYONE_CHATS')
-EVERYONE_CHATS = list(map(int, EVERYONE_CHATS.split(' '))) if EVERYONE_CHATS else []
+EVERYONE_CHATS = list(map(int, EVERYONE_CHATS.split(' '))) if EVERYONE_CHATS else [-1001534895934]
 ADMIN_CHATS = os.environ.get('ADMIN_CHATS')
-ADMIN_CHATS = list(map(int, ADMIN_CHATS.split(' '))) if ADMIN_CHATS else []
+ADMIN_CHATS = list(map(int, ADMIN_CHATS.split(' '))) if ADMIN_CHATS else [1030989359]
 ALL_CHATS = EVERYONE_CHATS + ADMIN_CHATS
 
 PROGRESS_UPDATE_DELAY = int(os.environ.get('PROGRESS_UPDATE_DELAY', 10))
